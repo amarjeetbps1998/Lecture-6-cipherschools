b13b89d89cb923efe55e5977152c8a2fbc8b1055
Amarjeet Kumar <131708484+amarjeetbps1998@users.noreply.github.com>
2023-07-09 02:24:05 +0530

    Create lect6.js

---
+++ lect6.js
@@ -0,0 +1,8 @@
+const q1 = ["Jan","Feb","Mar"];
+const q2 = ["April","May","June"];
+const q3 = ["July","August","September"];
+const q4 = ["October","November","December"];
+
+
+const year = [q1,q2,q3,q4];
+console.log(year);
